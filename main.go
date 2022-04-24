afb5b69dd94e4c9747a85351dc349ef3cee277c5
purplebro <467364280@qq.com>
2022-04-24 13:02:49 +0800

    comment db

--- main.go
+++ main.go
@@ -4,17 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"wxcloudrun-golang/db"
-	"wxcloudrun-golang/service"
+	// "wxcloudrun-golang/db"
+	// "wxcloudrun-golang/service"
 )
 
-func main() {
-	if err := db.Init(); err != nil {
-		panic(fmt.Sprintf("mysql init failed with %+v", err))
-	}
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
 
-	http.HandleFunc("/", service.IndexHandler)
-	http.HandleFunc("/api/count", service.CounterHandler)
+func main() {
+	// if err := db.Init(); err != nil {
+	// 	panic(fmt.Sprintf("mysql init failed with %+v", err))
+	// }
+
+	http.HandleFunc("/", handler)
+	// http.HandleFunc("/api/count", service.CounterHandler)
 
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
